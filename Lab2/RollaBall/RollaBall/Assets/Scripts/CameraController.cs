6c04ede71a9b6067544797fed211561ca1b95ad1
Nicole Dubin <nicole.dubin@gmail.com>
2018-02-05 07:56:22 -0800

    Updated camera controls

---
+++ Lab2/RollaBall/RollaBall/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject focus;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - focus.transform.position;
+	}
+	
+	// LateUpdate is called once per frame, but after everthing else(?)
+	void LateUpdate () {
+		transform.position = focus.transform.position + offset;
+	}
+}
